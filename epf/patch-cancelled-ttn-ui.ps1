025783ede9343f09da1b24492c5a869001a9f543
Nekitosha-IT <mrvendettabest@gmail.com>
2026-09-11 02:23:59 +0300

    Use unique form item IDs for UTM and TTN buttons

--- epf/patch-cancelled-ttn-ui.ps1
+++ epf/patch-cancelled-ttn-ui.ps1
@@ -7,11 +7,9 @@ $Form = Join-Path $EpRoot 'Forms\Форма\Ext\Form.xml'
 if (!(Test-Path -LiteralPath $Module)) { throw "Module not found: $Module" }
 if (!(Test-Path -LiteralPath $Form)) { throw "Form XML not found: $Form" }
 
-# 1. UTM /opt/out must be requested without forcing Accept: application/xml.
 $m = Get-Content -LiteralPath $Module -Raw -Encoding UTF8
 $m = $m.Replace('            Запрос.Заголовки.Вставить("Accept", "application/xml");' + [Environment]::NewLine, '')
 
-# 2. Add explicit UTM and TTN selection procedures once.
 if ($m -notmatch 'Процедура ВыбратьУТМ') {
 $insert = @'
 
@@ -73,31 +71,27 @@ $insert = @'
     $m = $m.Replace($marker, $insert + [Environment]::NewLine + $marker)
 }
 
-# 3. If a TTN was explicitly selected, correction buttons operate only on it.
 $m = $m.Replace('    Для каждого СтрокаТТН Из ТаблицаТТН Цикл' + [Environment]::NewLine + '        Если Не СтрокаТТН.Расхождение ИЛИ Не СтрокаТТН.ОтмененаПоставщиком Тогда', '    Для каждого СтрокаТТН Из ТаблицаТТН Цикл' + [Environment]::NewLine + '        Если Не ПустаяСтрока(ВыбраннаяТТН) И СтрокаТТН.НомерТТН <> ВыбраннаяТТН Тогда Продолжить; КонецЕсли;' + [Environment]::NewLine + '        Если Не СтрокаТТН.Расхождение ИЛИ Не СтрокаТТН.ОтмененаПоставщиком Тогда')
 $m = $m.Replace('    Для каждого СтрокаТТН Из ТаблицаТТН Цикл' + [Environment]::NewLine + '        Если Не СтрокаТТН.Расхождение ИЛИ Не СтрокаТТН.ОтмененаПоставщиком Тогда Продолжить; КонецЕсли;', '    Для каждого СтрокаТТН Из ТаблицаТТН Цикл' + [Environment]::NewLine + '        Если Не ПустаяСтрока(ВыбраннаяТТН) И СтрокаТТН.НомерТТН <> ВыбраннаяТТН Тогда Продолжить; КонецЕсли;' + [Environment]::NewLine + '        Если Не СтрокаТТН.Расхождение ИЛИ Не СтрокаТТН.ОтмененаПоставщиком Тогда Продолжить; КонецЕсли;')
-
 Set-Content -LiteralPath $Module -Value $m -Encoding UTF8
 
-# 4. Add form attributes and commands/buttons if they are not present.
 $f = Get-Content -LiteralPath $Form -Raw -Encoding UTF8
 if ($f -notmatch 'name="ВыбраннаяТТН"') {
     $attr = '    <Attribute name="ВыбраннаяТТН" id="8"><Title><v8:item><v8:lang>ru</v8:lang><v8:content>Выбранная ТТН</v8:content></v8:item></Title><Type><v8:Type>xs:string</v8:Type><v8:StringQualifiers><v8:Length>100</v8:Length><v8:AllowedLength>Variable</v8:AllowedLength></v8:StringQualifiers></Type></Attribute>'
     $f = $f.Replace('  </Attributes>' + [Environment]::NewLine, $attr + [Environment]::NewLine + '  </Attributes>' + [Environment]::NewLine)
 }
 if ($f -notmatch 'Command name="ВыбратьУТМ"') {
-    $buttons = '      <Button name="ВыбратьУТМ" id="4"><Type>UsualButton</Type><Title><v8:item><v8:lang>ru</v8:lang><v8:content>Выбрать УТМ</v8:content></v8:item></Title><CommandName>Form.Command.ВыбратьУТМ</CommandName></Button>' + [Environment]::NewLine +
-               '      <Button name="ПроверитьУТМ" id="5"><Type>UsualButton</Type><Title><v8:item><v8:lang>ru</v8:lang><v8:content>Проверить УТМ</v8:content></v8:item></Title><CommandName>Form.Command.ПроверитьУТМ</CommandName></Button>' + [Environment]::NewLine +
-               '      <Button name="ВыбратьТТН" id="6"><Type>UsualButton</Type><Title><v8:item><v8:lang>ru</v8:lang><v8:content>Выбрать ТТН</v8:content></v8:item></Title><CommandName>Form.Command.ВыбратьТТН</CommandName></Button>' + [Environment]::NewLine
+    $buttons = '      <Button name="ВыбратьУТМ" id="30"><Type>UsualButton</Type><Title><v8:item><v8:lang>ru</v8:lang><v8:content>Выбрать УТМ</v8:content></v8:item></Title><CommandName>Form.Command.ВыбратьУТМ</CommandName></Button>' + [Environment]::NewLine +
+               '      <Button name="ПроверитьУТМ" id="31"><Type>UsualButton</Type><Title><v8:item><v8:lang>ru</v8:lang><v8:content>Проверить УТМ</v8:content></v8:item></Title><CommandName>Form.Command.ПроверитьУТМ</CommandName></Button>' + [Environment]::NewLine +
+               '      <Button name="ВыбратьТТН" id="32"><Type>UsualButton</Type><Title><v8:item><v8:lang>ru</v8</v8:lang><v8:content>Выбрать ТТН</v8:content></v8:item></Title><CommandName>Form.Command.ВыбратьТТН</CommandName></Button>' + [Environment]::NewLine
     $needle = '      <Button name="Получить" id="5">'
     $f = $f.Replace($needle, $buttons + $needle)
 }
 if ($f -notmatch 'Command name="ВыбратьУТМ"') {
-    $commands = '    <Command name="ВыбратьУТМ" id="5"><Title><v8:item><v8:lang>ru</v8:lang><v8:content>Выбрать УТМ</v8:content></v8:item></Title><Action>ВыбратьУТМ</Action><CurrentRowUse>DontUse</CurrentRowUse></Command>' + [Environment]::NewLine +
-                '    <Command name="ПроверитьУТМ" id="6"><Title><v8:item><v8:lang>ru</v8:lang><v8:content>Проверить УТМ</v8:content></v8:item></Title><Action>ПроверитьУТМ</Action><CurrentRowUse>DontUse</CurrentRowUse></Command>' + [Environment]::NewLine +
-                '    <Command name="ВыбратьТТН" id="7"><Title><v8:item><v8:lang>ru</v8:lang><v8:content>Выбрать ТТН</v8:content></v8:item></Title><Action>ВыбратьТТН</Action><CurrentRowUse>DontUse</CurrentRowUse></Command>' + [Environment]::NewLine
+    $commands = '    <Command name="ВыбратьУТМ" id="30"><Title><v8:item><v8:lang>ru</v8:lang><v8:content>Выбрать УТМ</v8:content></v8:item></Title><Action>ВыбратьУТМ</Action><CurrentRowUse>DontUse</CurrentRowUse></Command>' + [Environment]::NewLine +
+                '    <Command name="ПроверитьУТМ" id="31"><Title><v8:item><v8:lang>ru</v8:lang><v8:content>Проверить УТМ</v8:content></v8:item></Title><Action>ПроверитьУТМ</Action><CurrentRowUse>DontUse</CurrentRowUse></Command>' + [Environment]::NewLine +
+                '    <Command name="ВыбратьТТН" id="32"><Title><v8:item><v8:lang>ru</v8:lang><v8:content>Выбрать ТТН</v8:content></v8:item></Title><Action>ВыбратьТТН</Action><CurrentRowUse>DontUse</CurrentRowUse></Command>' + [Environment]::NewLine
     $f = $f.Replace('  </Commands>' + [Environment]::NewLine, $commands + '  </Commands>' + [Environment]::NewLine)
 }
 Set-Content -LiteralPath $Form -Value $f -Encoding UTF8
-
 Write-Host 'PATCH OK: UTM 406 fix + UTM/TTN buttons + selected TTN mode.' -ForegroundColor Green
